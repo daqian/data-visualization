4121d4a3cdf7b47a34b49dc42f45314ed2768003
lemisu <369744853@qq.com>
2022-10-10 19:05:52 +0800

    add curve chart

--- src/components/CurveChart.vue
+++ src/components/CurveChart.vue
@@ -4,76 +4,99 @@ import * as d3 from "d3";
 export default {
   mounted() {
     const width = 300;
-       const height = 250;
-      const margin = {left:50,top:30,right:20,bottom:20};
-// 图表的宽度=svg的宽度减去margin
-       const g_width = width - margin.left-margin.right;
-        const g_height = height - margin.top - margin.bottom;
+    const height = 250;
+    const data = [
+      { displacement: 0, redHeight: 100 },
+      { displacement: 1, redHeight: 50 },
+      { displacement: 2, redHeight: 10 },
+      { displacement: 3, redHeight: 5 },
+      { displacement: 4, redHeight: 2 },
+      { displacement: 5, redHeight: 1 },
+      { displacement: 6, redHeight: 0.5 },
+    ];
+
     const svg = d3.select("#j-curve-chart-svg").attr("width", width).attr("height", height);
+    const g = svg.append("g");
 
 
-    const g = svg.append("g")
-        // 设置x,y轴偏移量
-        .attr("transform","translate(" +margin.left+","+ margin.top +")")
-
-    const data = [1,3,5,7,8,4,3,7]
-
-// 设置缩放
-    const scale_x = d3.scaleLinear().domain([0,data.length-1]).range([0,g_width])
-    const scale_y = d3.scaleLinear().domain([0,d3.max(data)]).range([g_height,0])
-
-    const line_generator = d3.line()
-        // d表示传进来的数据 i表示数据的下标
-        .x(function(d,i){return scale_x(i);}) // 0,1,2,3
-        .y(function(d){return scale_y(d);}); // 1,3,5
-        // 去除线的棱角 使其顺滑
-        // .interpolate("cardinal")
-
-    g.append("path")
-        // d 是 path data的缩写 将data数据传人
-        .attr("d",line_generator(data)) // d = "M1,0L20,40L40,50L100,100L0,200"
+    //3. Creating the Chart Axes
     const x = d3
         .scaleTime()
         .domain(
             d3.extent(data, function (d) {
-              return d;
+              return d.displacement;
             })
         )
-        .rangeRound([0, width]);
+        .rangeRound([0, width - 60]);
 
     const y = d3
         .scaleLinear()
         .domain(
             d3.extent(data, function (d) {
-              return d.amount;
+              return d.redHeight;
             })
         )
-        .rangeRound([height, 0]);
+        .rangeRound([height - 60, 0]);
 
+    const lineX = d3
+        .scaleTime()
+        .domain(
+            d3.extent(data, function (d) {
+              return d.displacement;
+            })
+        )
+        .rangeRound([60, width - 60]);
+
+    const lineY = d3
+        .scaleLinear()
+        .domain(
+            d3.extent(data, function (d) {
+              return d.redHeight;
+            })
+        )
+        .rangeRound([height - 60, 20]);
     //4. Creating a Line
-    const line = d3
+    const firstLine = d3
         .line()
+        .curve(d3.curveCatmullRom)
         .x(function (d) {
-          return x(parseTime(d.date));
+          return lineX(d.displacement);
         })
         .y(function (d) {
-          return y(d.amount);
+          return lineY(d.redHeight);
         });
 
+    //5. Appending the Axes to the Chart
     g.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
+        .attr("transform", "translate(30," + (height - 40) + ")")
+        .call(d3.axisBottom(x))
+        .append("text")
+        .attr("fill", "#000")
+        // .attr("x", 6)
+        .attr("dx", width - 40)
+        .attr("dy", "-0.71em")
+        .attr("text-anchor", "end")
+        .text("排量(L/S)");
 
     g.append("g")
+        .attr("transform", "translate(30, 20)")
         .call(d3.axisLeft(y))
         .append("text")
         .attr("fill", "#000")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
+        // .attr("transform", "rotate(-90)")
+        .attr("y", -18)
+        // .attr("dx", "0.71em")
         .attr("dy", "0.71em")
-        .attr("text-anchor", "end")
-        .text("Price ($)");
+        .attr("text-anchor", "middle")
+        .text("高度(cm)");
 
+    //6. Appending a path to the Chart
+    g.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", firstLine);
   },
 };
 </script>
