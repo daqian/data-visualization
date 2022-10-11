3ae4fbf7c8724756dd0a4b6a7b1e481153578f2a
lemisu <369744853@qq.com>
2022-10-11 10:00:36 +0800

    add element and header condition and layout

---
+++ src/components/HeaderCondition.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import { ElSelectV2, ElButton, ElDropdown, ElDropdownMenu, ElDropdownItem, ElIcon, ElButtonGroup } from 'element-plus'
+import type {SelectOption} from "@/lib/interfaces";
+
+const value: Ref<number> = ref<number>(0)
+const options: SelectOption[] = [
+  {
+    value: 0,
+    label: '主井筒'
+  },
+  {
+    value: 1,
+    label: "井筒2"
+  }
+]
+
+
+</script>
+
+<template>
+  <div class="header-condition">
+    <div class="condition-element">
+      <el-button-group>
+        <el-button type="primary" class="toggle-button active">历史</el-button>
+        <el-button type="primary" class="toggle-button">
+          实时
+        </el-button>
+      </el-button-group>
+    </div>
+    <div class="condition-element">
+      <el-select-v2
+          v-model="value"
+          :options="options"
+          placeholder="Please select"
+          size="large"
+      />
+    </div>
+    <div>
+
+      <el-dropdown>
+    <span class="el-dropdown-link">
+      图像选择
+      <el-icon class="el-icon--right">
+        <arrow-down />
+      </el-icon>
+    </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>Action 1</el-dropdown-item>
+            <el-dropdown-item>Action 2</el-dropdown-item>
+            <el-dropdown-item>Action 3</el-dropdown-item>
+            <el-dropdown-item disabled>Action 4</el-dropdown-item>
+            <el-dropdown-item divided>Action 5</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.header-condition {
+  display: flex;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.condition-element {
+  margin-left: 10px;
+
+}
+  .toggle-button {
+    margin: 0;
+    border-radius: 0;
+    border: solid 1px #444;
+    background-color: #ffffff;
+    color: #444;
+  }
+  .toggle-button.active {
+    background-color: #444;
+    color: #ffffff;
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import HeaderCondition from './../components/HeaderCondition.vue'
 import CurveChart from './../components/CurveChart.vue'
 import MultipleLineChart from './../components/MultipleLineChart.vue'
 import LineChart from './../components/LineChart.vue'
@@ -7,38 +8,62 @@ import MultipleLineChartTwo from './../components/MultipleLineChartTwo.vue'
 import MultipleLineChartThree from './../components/MultipleLineChartThree.vue'
 </script>
 <template>
-  <div class="chart-wrapper">
-    <div class="chart-box">
-      <CurveChart />
-    </div>
-    <div class="chart-box">
-      <MultipleLineChart />
-    </div>
-    <div class="chart-box">
-      <LineChart />
-    </div>
-    <div class="chart-box">
-      <MultipleLineChartOne />
-    </div>
-    <div class="chart-box">
-      <MultipleLineChartTwo />
-    </div>
-    <div class="chart-box">
-      <MultipleLineChartThree />
+  <div class="wrapper">
+    <HeaderCondition />
+    <div class="content">
+      <div class="chart-wrapper">
+        <div class="chart-box">
+          <CurveChart />
+        </div>
+        <div class="chart-box">
+          <MultipleLineChart />
+        </div>
+        <div class="chart-box">
+          <LineChart />
+        </div>
+        <div class="chart-box">
+          <MultipleLineChartOne />
+        </div>
+        <div class="chart-box">
+          <MultipleLineChartTwo />
+        </div>
+        <div class="chart-box">
+          <MultipleLineChartThree />
+        </div>
+      </div>
+      <div class="right-side">
+        <div class="param-config">Param Config</div>
+        <div class="hole-size">Hole Size</div>
+      </div>
     </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .chart-wrapper {
-    width: 1200px;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-  }
-  .chart-box {
-    margin: 20px;
-  }
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+}
+.content {
+  display: flex;
+}
+.chart-wrapper {
+  width: 1200px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+.right-side {
+  display: flex;
+  flex-direction: column;
+}
+.param-config {
+  height: 200px;
+}
+.hole-size {
+
+}
+.chart-box {
+  margin: 20px;
 }
 </style>
