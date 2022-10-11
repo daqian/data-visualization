d4a3ab9c80c11b416d6cea1d5de849cad591cc0a
lemisu <369744853@qq.com>
2022-10-11 11:33:06 +0800

    show and hide chart by condition change

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,9 +6,15 @@ import LineChart from './../components/LineChart.vue'
 import MultipleLineChartOne from './../components/MultipleLineChartOne.vue'
 import MultipleLineChartTwo from './../components/MultipleLineChartTwo.vue'
 import MultipleLineChartThree from './../components/MultipleLineChartThree.vue'
+import {ref, Ref} from "vue";
+import {DataTypeValue} from "@/lib/enums";
 
+let selectImageValues: Ref<number[]> = ref<number[]>([0, 1, 2, 3, 4, 5, 6]);
 const onConditionChange = function (condition: any) {
   console.log(condition);
+  if (condition.selectImageValues) {
+    selectImageValues.value = condition.selectImageValues;
+  }
 }
 </script>
 <template>
@@ -16,27 +22,27 @@ const onConditionChange = function (condition: any) {
     <HeaderCondition @onConditionChange="onConditionChange" />
     <div class="content">
       <div class="chart-wrapper">
-        <div class="chart-box">
+        <div class="chart-box" v-if="selectImageValues.includes(0)">
           <CurveChart />
           <div>最小循环周</div>
         </div>
-        <div class="chart-box">
+        <div class="chart-box" v-if="selectImageValues.includes(1)">
           <MultipleLineChart />
           <div>最小循环时间</div>
         </div>
-        <div class="chart-box">
+        <div class="chart-box" v-if="selectImageValues.includes(2)">
           <LineChart />
           <div>岩屑床高度</div>
         </div>
-        <div class="chart-box">
+        <div class="chart-box" v-if="selectImageValues.includes(3)">
           <MultipleLineChartOne />
           <div>环空流速</div>
         </div>
-        <div class="chart-box">
+        <div class="chart-box" v-if="selectImageValues.includes(4)">
           <MultipleLineChartTwo />
           <div>携岩指数（CCI）</div>
         </div>
-        <div class="chart-box">
+        <div class="chart-box" v-if="selectImageValues.includes(5)">
           <MultipleLineChartThree />
           <div>岩屑浓度百分比</div>
         </div>
