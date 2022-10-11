432c3895711d3dbfdf89bc593097639338f59783
lemisu <369744853@qq.com>
2022-10-11 09:04:47 +0800

    delete demo file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,44 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import CurveChart from './../components/CurveChart.vue'
+import MultipleLineChart from './../components/MultipleLineChart.vue'
+import LineChart from './../components/LineChart.vue'
+import MultipleLineChartOne from './../components/MultipleLineChartOne.vue'
+import MultipleLineChartTwo from './../components/MultipleLineChartTwo.vue'
+import MultipleLineChartThree from './../components/MultipleLineChartThree.vue'
 </script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="chart-wrapper">
+    <div class="chart-box">
+      <CurveChart />
+    </div>
+    <div class="chart-box">
+      <MultipleLineChart />
+    </div>
+    <div class="chart-box">
+      <LineChart />
+    </div>
+    <div class="chart-box">
+      <MultipleLineChartOne />
+    </div>
+    <div class="chart-box">
+      <MultipleLineChartTwo />
+    </div>
+    <div class="chart-box">
+      <MultipleLineChartThree />
+    </div>
+  </div>
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .chart-wrapper {
+    width: 1200px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+  .chart-box {
+    margin: 20px;
+  }
+}
+</style>
